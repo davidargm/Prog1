0cc20f8b9393c7b8a0407fba1a4de94b5dfce95c
davidargm <72325721+davidargm@users.noreply.github.com>
2021-06-14 16:13:00 -0300

    Clase7_19

--- Clase7/Clase7_19/Main.java
+++ Clase7/Clase7_19/Main.java
@@ -13,132 +13,154 @@ import java.io.InputStreamReader;
 import java.util.Random;
 
 public class Main {
-	public static int MAX = 20;
-	public static int MAXVALOR = 9;
-	public static int MINVALOR = 1;
+	public static final int MAX = 20;
+	public static final int MAXVALOR = 9;
+	public static final int MINVALOR = 1;
 	public static final double probabilidad_numero = 0.4;
 	public static void main(String[] args) {
+		int[] ORIGINAL = new int[MAX];
+		int[] ORDEN1 = new int[MAX];
 		
-		int [] arrenteros = new int [MAX];
-		int [] orden1 = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,};
-		int indice = 0;
-		int posicion = 0;
-		int inicio = 0;
-		int fin = 0;
-		int i = 0;
+		cargar_arreglo_aleatorio_secuencias_int(ORIGINAL);
+		imprimir_arreglo_secuencias_int(ORIGINAL);
+		inicializar_arreglo(ORDEN1);
+		buscarSecuenciasPares(ORIGINAL,ORDEN1);
+		imprimir_arreglo_secuencias_int(ORDEN1);
 		
-		cargar_arreglo_aleatorio_secuencias_int(arrenteros);
-		imprimir_arreglo_secuencias_int(arrenteros);
-		
-		while (i < MAX-1){
-			if (arrenteros[i]==0){
-				i++;
-			}
-			else {
-				inicio = i;
-				fin = buscarFinSecuencia(arrenteros, inicio);
-				int suma = sumarSecuencia(arrenteros, inicio);
-				i = fin+1;
-				if (suma%2==0){
-					insertarInicio(arrenteros,orden1, inicio);
+		eliminarSecuencia(ORIGINAL,ORDEN1);
+	}
+	
+	public static void eliminarSecuencia(int[]arr, int[]ind){
+		int pos = getInput();
+		if(pos>=0 && pos<MAX){
+			int inicio = buscarInicio(arr,(pos-1));
+			if(inicio==pos){
+				int fin=buscarFin(arr,pos);
+				for(int i=pos; i<=fin; i++){
+					corrimiento_izq(arr,pos);
 				}
-			}
-		}
-		imprimir_arreglo_secuencias_int(orden1);
-		posicion = getPosicionSecuencia();
-		for(int k = 0; k<MAX-1; k++){
-			if(posicion==arrenteros[k]){
-				borrarSecuencia(arrenteros, posicion);
-				borrarIndice(orden1,posicion);
-				imprimir_arreglo_secuencias_int(arrenteros);
-				imprimir_arreglo_secuencias_int(orden1);
-			}
-			else{
-				System.out.println("La posicion no corresponde al comienzo de una secuencia");
-			}
-		}
-	}
-	
-	public static void borrarIndice(int[] ind, int pos){
-		int j = pos;
-		while (pos<MAX-1){
-			ind[j] = ind[j+1];
-			j++;
-		}
-	}
-	
-	
-	public static void borrarSecuencia(int [] arrenteros, int pos){
-		int m = pos;
-		while(arrenteros[pos] < MAX && arrenteros[pos]!=0){
-			for(int n = pos; n<(MAX-1); n++){
-				arrenteros[n]=arrenteros[n+1];
-			}
-		}
-	}
-	
-	public static int getPosicionSecuencia(){
-		BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-		int input = 0;
-		try{
-			System.out.println("Ingrese una posicion del arreglo: ");
-			input = Integer.valueOf(entrada.readLine());
-		}
-		catch (Exception exc){
-			System.out.println("Ingresaste cualquier verdura");
-		}
-		return input;
-	}
-	
-	public static void insertarInicio(int[] arrenteros, int[] indiceSumaPar, int inicio){
-		int posicion = buscarPosicionInd(arrenteros, indiceSumaPar, inicio);
-		corrimiento_der(indiceSumaPar, posicion);
-		indiceSumaPar[posicion]= inicio;
-	}
-	
-	public static int buscarPosicionInd(int[] arrenteros, int[] ind, int posicion){
-		int pos = 0;
-		int sumaInd = 0;
-		int sumaSec = 0;
-		while((pos < MAX-1) && (ind[pos] != -1) && sumaInd>sumaSec){
-			int inicioInd = ind[pos];
-			sumaInd = sumarSecuencia(arrenteros, inicioInd);
-			sumaSec = sumarSecuencia(arrenteros, posicion);
-			if(sumaInd<sumaSec){
-				pos++;
+				
+				imprimir_arreglo_secuencias_int(arr);
+				inicializar_arreglo(ind);
+				buscarSecuenciasPares(arr,ind);
+				imprimir_arreglo_secuencias_int(ind);
 			}
 			
+			else{
+				System.out.println("La posicion ingresada no corresponde al inicio de una secuencia");
+			}
+		}
+		
+		else{
+			System.out.println("La posicion no esta dentro del rango del arreglo");
+		}
+	}
+	
+	public static int getInput(){
+		BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+		int pos = 0;
+		try{
+			System.out.println("Ingrese la posicion de inicio de la secuencia que desea eliminar");
+			pos = Integer.valueOf(entrada.readLine());
+		}
+		catch (Exception exc){
+			System.out.println(exc);
 		}
 		return pos;
 	}
 	
-	public static int sumarSecuencia(int [] arr, int inicio){
-		int suma = 0;
-		int i = inicio;
-		while(i<MAX && arr[i]!= 0){
-			suma += arr[i];
-			i++;
+	public static void buscarSecuenciasPares(int[]arr, int[]ind){
+		int inicio = 0;
+		int fin = 0;
+		while(inicio<MAX-1){
+			int suma = 0;
+			inicio=buscarInicio(arr,inicio);
+			fin=buscarFin(arr,inicio);
+			suma=sumarSecuencia(arr,inicio);
+			if(suma%2==0 && suma!=0){
+				agregarIndice(arr,ind,inicio,suma);
+			}
+			inicio=fin+1;
 		}
-		return suma;
+		
 	}
 	
-	public static int buscarFinSecuencia(int [] arr, int pos){
-		int i = pos;
-		while(arr[i]!=0){
-			i++;			
+	public static void agregarIndice(int[] arr, int[] ind, int inicio, int suma1){
+		int i=0;
+		int encontrados = 0;
+		while(i<MAX  && encontrados==0){
+			if(ind[i]==-1){
+				ind[i]=inicio;
+				i=MAX;
+			}
+			else{
+				int suma2 = sumarSecuencia(arr, ind[i]);
+				if(suma1<suma2){
+					corrimiento_der(ind,i);
+					ind[i]=inicio;
+					encontrados++;
+				}
+				else{
+					i++;
+				}
+			}
 		}
-		return i-1;
 	}
 	
-	public static void corrimiento_der(int[] ind, int posicion) {
+	
+	public static void corrimiento_der(int[] arr,int pos) {
 		int actual = MAX-1;
-		while (posicion<actual){
-			ind[actual] = ind[actual-1];
+		while (pos<actual){
+			arr[actual] = arr[actual-1];
 			actual--;
 		}
 	}
 	
-	public static void cargar_arreglo_aleatorio_secuencias_int(int []arr){
+	public static void corrimiento_izq(int[] arr,int pos) {
+		while (pos<MAX-1){
+			arr[pos] = arr[pos+1];
+			pos++;
+		}
+	} 
+
+	
+	public static int sumarSecuencia(int[]arrint, int inicio){
+		int total = 0;
+		int fin = buscarFin(arrint, inicio);
+		for(int i=inicio;i<=fin;i++){
+			total += arrint[i];
+		}
+		return total;
+	}
+	
+	public static int buscarInicio(int []arrint, int pos){
+		int i = pos;
+		while(arrint[i]==0 && i<MAX-1){
+			i++;
+		}
+		return i;
+	}
+	
+	public static int buscarFin(int[]arrint, int pos){
+		while(arrint[pos]!=0 && pos<MAX-1){
+			pos++;
+		}
+		if(pos==MAX-1){
+			return pos;
+		}
+		else{
+			return pos-1;
+		}
+		
+	}
+	
+	public static void inicializar_arreglo(int [] arr){
+		for (int pos = 0; pos < MAX; pos++){
+			arr[pos] = -1;
+		}
+	} 
+	
+	public static void cargar_arreglo_aleatorio_secuencias_int(int[]arr){
 		Random r = new Random();
 		arr[0] = 0;
 		arr[MAX-1] = 0;
@@ -151,14 +173,12 @@ public class Main {
 			}
 		}
 	}
-
-	//imprimir arreglo aleatorio de secuencias int
+	
 	public static void imprimir_arreglo_secuencias_int(int [] arr){
 		System.out.print("Arreglo de secuencias int\n|");
 		for (int pos = 0; pos < MAX; pos++){
 			System.out.print(arr[pos]+"|");
 		}
-		System.out.print("\n");		
+		System.out.print("\n");
 	}
-
 }
